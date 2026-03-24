3ba6f4e90b89798f6cf75c184971f7b30f4d3e8d
Gabriel-Alexandre-R-Pereira <profissionalgabriel96@gmail.com>
2026-03-23 23:20:21 -0300

    Ortografia corrigida em prova04.py

--- prova04.py
+++ prova04.py
@@ -5,17 +5,17 @@ def media(num1, num2, num3):
     return media_aritimetica
 
 teste1 = media(8, 9, 10)
-print(f"A média aritimética de 8, 9 e 10 é: {teste1}")
+print(f"A média aritmética de 8, 9 e 10 é: {teste1}")
 
 numero1 = 80
 numero2 = 90
 numero3 = 100
 teste2 = media(numero1, numero2, numero3)
-print(f"A média aritimética de 80, 90 e 100 é: {teste2}")
+print(f"A média aritmética de 80, 90 e 100 é: {teste2}")
 
 lista_numeros = []
 for numero in range(3):
-    numero = float(input("Dgite um número: "))
+    numero = float(input("Digite um número: "))
     lista_numeros.append(numero)
 teste3 = media(*lista_numeros)
-print(f"A média aritimética de {*lista_numeros,}: {teste3}")
+print(f"A média aritmética de {*lista_numeros,}: {teste3}")
